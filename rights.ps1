58f0fef361f25d171b4bdbf27979cb5b7dab1d48
merhabasii <merhabasi@tutamail.com>
2026-05-21 02:03:33 +0300

    Update rights.ps1

--- rights.ps1
+++ rights.ps1
@@ -29,9 +29,7 @@ $filePaths = @(
 )
 
 foreach ($filePath in $filePaths) {
-    if (-not (Test-Path $filePath)) {
-        continue
-    }
+    if (-not (Test-Path $filePath)) { continue }
     
     [System.IO.File]::SetAttributes($filePath, [System.IO.FileAttributes]::Normal)
 
@@ -39,28 +37,17 @@ foreach ($filePath in $filePaths) {
     $systemAccount = New-Object System.Security.Principal.NTAccount("NT AUTHORITY\SYSTEM")
     $aclSettings.SetOwner($systemAccount)
 
+
     $aclSettings.SetAccessRuleProtection($true, $false)
+
 
     $systemAccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("NT AUTHORITY\SYSTEM", "FullControl", "Allow")
     $aclSettings.AddAccessRule($systemAccessRule)
 
-    $adminGroup = New-Object System.Security.Principal.NTAccount("BUILTIN\Administrators")
-    $readRights = [System.Security.AccessControl.FileSystemRights]::ReadAndExecute -bor [System.Security.AccessControl.FileSystemRights]::Read -bor [System.Security.AccessControl.FileSystemRights]::Synchronize
-    $adminReadRule = New-Object System.Security.AccessControl.FileSystemAccessRule($adminGroup, $readRights, "Allow")
-    $aclSettings.AddAccessRule($adminReadRule)
 
-    $denyRights = [System.Security.AccessControl.FileSystemRights]::Delete -bor `
-                  [System.Security.AccessControl.FileSystemRights]::Write -bor `
-                  [System.Security.AccessControl.FileSystemRights]::DeleteSubdirectoriesAndFiles -bor `
-                  [System.Security.AccessControl.FileSystemRights]::WriteAttributes -bor `
-                  [System.Security.AccessControl.FileSystemRights]::WriteExtendedAttributes -bor `
-                  [System.Security.AccessControl.FileSystemRights]::ChangePermissions -bor `
-                  [System.Security.AccessControl.FileSystemRights]::TakeOwnership
-
-    $adminDenyRule = New-Object System.Security.AccessControl.FileSystemAccessRule($adminGroup, $denyRights, "Deny")
-    $aclSettings.AddAccessRule($adminDenyRule)
 
     Set-Acl -Path $filePath -AclObject $aclSettings -ErrorAction SilentlyContinue
+
 
     [System.IO.File]::SetAttributes($filePath, [System.IO.FileAttributes]::ReadOnly -bor [System.IO.FileAttributes]::System)
 }
